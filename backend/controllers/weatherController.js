beb96100a488b5b515edfea12c2a0ca9cb0fad07
MilkJJ <iwannoverify@gmail.com>
2023-07-01 19:15:03 +0800

    weather info

--- backend/controllers/weatherController.js
+++ backend/controllers/weatherController.js
@@ -30,18 +30,45 @@ const getWeather = async (req, res) => {
 
 // create new weather
 const createWeather = async (req, res) => {
-  const {title, load, reps} = req.body
+  const {latitude, longitude, cloud_pct, temp, feels_like, humidity, min_temp, max_temp, wind_speed, wind_degrees, sunrise, sunset} = req.body
 
   let emptyFields = []
 
-  if(!title) {
-    emptyFields.push('title')
+  if(!latitude) {
+    emptyFields.push('latitude')
   }
-  if(!load) {
-    emptyFields.push('load')
+  if(!longitude) {
+    emptyFields.push('longitude')
   }
-  if(!reps) {
-    emptyFields.push('reps')
+  if(!cloud_pct) {
+    emptyFields.push('cloud_pct')
+  }
+  if(!temp) {
+    emptyFields.push('temp')
+  }
+  if(!feels_like) {
+    emptyFields.push('feels_like')
+  }
+  if(!humidity) {
+    emptyFields.push('humidity')
+  }
+  if(!min_temp) {
+    emptyFields.push('min_temp')
+  }
+  if(!max_temp) {
+    emptyFields.push('max_temp')
+  }
+  if(!wind_speed) {
+    emptyFields.push('wind_speed')
+  }
+  if(!wind_degrees) {
+    emptyFields.push('wind_degrees')
+  }
+  if(!sunrise) {
+    emptyFields.push('sunrise')
+  }
+  if(!sunset) {
+    emptyFields.push('sunset')
   }
   if(emptyFields.length > 0) {
     return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
@@ -50,7 +77,7 @@ const createWeather = async (req, res) => {
   // add doc to db
   try {
     const user_id = req.user._id
-    const weather = await Weather.create({title, load, reps, user_id})
+    const weather = await Weather.create({latitude, longitude, cloud_pct, temp, feels_like, humidity, min_temp, max_temp, wind_speed, wind_degrees, sunrise, sunset, user_id})
     res.status(200).json(weather)
   } catch (error) {
     res.status(400).json({error: error.message})
